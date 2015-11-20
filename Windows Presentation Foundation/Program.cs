69afdb0cd68f2dccb406e6857faeafe29d70d3ae
lester88a <lester88a@gmail.com>
2015-11-20 15:32:40 -0500

    1

---
+++ Windows Presentation Foundation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Presentation_Foundation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  WPF 
+             *  windos presentation foundation
+             *  GUI: windows forms & WPF
+             *  
+             * WHY?
+             * ~ Tier separation
+             * ~ WPF separats interface design for logic> XAML (Extended application markup language).
+             * Characteristics: 
+             *      ~separates design from logic
+             *      ~vector_based graphics as oppened to [raster_based> bitmaps]
+             *      ~resolution independent
+             * 
+             * 3 tier application: interface(), Logic(code), Store(DB).
+             * 
+             * 
+             * Layout containers:
+             *  Grid: grid_based
+             *  Canvas: Co-ordinates-based
+             *  StackPanel: vertical or horizontal orientation
+             *  WrapPanel:
+             *  
+             *  
+             * 
+             */
+        }
+    }
+}
